c376428d5512fce5848c5dcfabfc40eb49e962b2
hy <1052586315@qq.com>
2023-03-04 10:31:51 +0800

    1

---
+++ src/main/java/com/hy/lwmsbackend/LwmsBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.hy.lwmsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LwmsBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LwmsBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hy/lwmsbackend/LwmsBackEndApplicationTests.java
@@ -0,0 +1,15 @@
+package com.hy.lwmsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootTest
+class LwmsBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
